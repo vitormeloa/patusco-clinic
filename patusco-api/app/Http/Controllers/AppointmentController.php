cc6c15cfb1b5364f8633c90685807ac682dd8a8a
Vitor Melo <melo.vitor2001@gmail.com>
2024-09-10 00:59:24 -0300

    wip: updating controller

--- patusco-api/app/Http/Controllers/AppointmentController.php
+++ patusco-api/app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UpdateAppointmentRequest;
 use App\Models\Appointment;
 use App\Services\AppointmentService;
 use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Gate;
 
 class AppointmentController extends Controller
@@ -20,26 +21,28 @@ class AppointmentController extends Controller
         $this->appointmentService = $appointmentService;
     }
 
-    public function index()
+    public function index(): JsonResponse
     {
         $appointments = $this->appointmentService->getAppointments();
         return $this->success($appointments);
     }
 
-    public function store(CreateAppointmentRequest $request)
+    public function store(CreateAppointmentRequest $request): JsonResponse
     {
+        Gate::authorize('create', Appointment::class);
         $appointment = $this->appointmentService->createAppointment($request->validated());
         return $this->created($appointment);
     }
 
-    public function update(UpdateAppointmentRequest $request, Appointment $appointment)
+
+    public function update(UpdateAppointmentRequest $request, Appointment $appointment): JsonResponse
     {
         Gate::authorize('update', $appointment);
         $appointment = $this->appointmentService->updateAppointment($appointment, $request->validated());
         return $this->updated($appointment);
     }
 
-    public function destroy(Appointment $appointment)
+    public function destroy(Appointment $appointment): JsonResponse
     {
         Gate::authorize('delete', $appointment);
         $this->appointmentService->deleteAppointment($appointment);
