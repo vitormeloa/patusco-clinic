27c9e075ab35bde807f854b135464238deba30ed
Vitor Melo <melo.vitor2001@gmail.com>
2024-09-09 22:44:49 -0300

    wip: updating routes

--- patusco-api/routes/api.php
+++ patusco-api/routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+require __DIR__ . '/auth.php';
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/appointments', [AppointmentController::class, 'index']);
+    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::put('/appointments/{appointment}', [AppointmentController::class, 'update']);
+    Route::delete('/appointments/{appointment}', [AppointmentController::class, 'destroy']);
 });
